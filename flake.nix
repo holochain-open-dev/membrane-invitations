d407d371cdafd45bf5d03da9cbc1ed6e9e427350
pospi <pospi@spadgos.com>
2023-12-05 18:13:58 +1100

    set Holochain dependencies for latest 0.3 beta

--- flake.nix
+++ flake.nix
@@ -1,39 +1,44 @@
 {
   description = "Template for Holochain app development";
-  
+
   inputs = {
-    nixpkgs.follows = "holochain/nixpkgs";
+    versions.url  = "github:holochain/holochain/holochain-0.3.0-beta-dev.26?dir=versions/weekly";
 
-    versions.url = "github:holochain/holochain?dir=versions/0_2";
+    holochain-flake.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.26";
+    holochain-flake.inputs.versions.follows = "versions";
+    holochain-flake.inputs.holochain.url = "github:holochain/holochain/holochain-0.3.0-beta-dev.26";
 
-    holochain = {
-      url = "github:holochain/holochain";
-      inputs.versions.follows = "versions";
-    };
+    nixpkgs.follows = "holochain-flake/nixpkgs";
+    flake-parts.follows = "holochain-flake/flake-parts";
   };
 
-  outputs = inputs @ { ... }:
-    inputs.holochain.inputs.flake-parts.lib.mkFlake
+  outputs = inputs:
+    inputs.flake-parts.lib.mkFlake
       {
         inherit inputs;
       }
       {
-        systems = builtins.attrNames inputs.holochain.devShells;
+        systems = builtins.attrNames inputs.holochain-flake.devShells;
         perSystem =
           { inputs'
           , config
           , pkgs
           , system
-          , lib
           , ...
           }: {
             devShells.default = pkgs.mkShell {
-              inputsFrom = [ inputs'.holochain.devShells.holonix ];
+              inputsFrom = [ inputs'.holochain-flake.devShells.holonix ];
               packages = with pkgs; [
                 nodejs-18_x
+                nodePackages.pnpm
                 # more packages go here
                 cargo-nextest
               ];
+
+              shellHook = ''
+                unset CARGO_TARGET_DIR
+                unset CARGO_HOME
+              '';
             };
           };
       };
